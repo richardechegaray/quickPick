8862214b1194c1029ab7892d2f1a389910e031cc
Paul Lin <linpaul0328@gmail.com>
2020-10-17 14:06:36 -0700

    Add buttons to main screen
    Buttons have been added to the main screen and registered in the
    corresponding Java file.

--- frontend/app/src/main/java/com/quickpick/MainActivity.java
+++ frontend/app/src/main/java/com/quickpick/MainActivity.java
@@ -3,12 +3,25 @@ package com.quickpick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createNewList, viewEditLists, joinSession, viewOldSessions, createSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButtons();
+    }
+
+    private void registerButtons() {
+        createNewList = findViewById(R.id.create_new_list_button);
+        viewEditLists = findViewById(R.id.view_edit_lists_button);
+        joinSession = findViewById(R.id.join_session_button);
+        viewOldSessions = findViewById(R.id.view_old_sessions_button);
+        createSession = findViewById(R.id.create_session_button);
     }
 }
