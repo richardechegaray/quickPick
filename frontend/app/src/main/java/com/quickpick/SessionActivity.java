309e3717028d03d52a3b4ec159fd233771c970c7
Richard Echegaray <richardechegaray@outlook.com>
2020-10-21 22:41:10 -0700

    added buttons in sessionview that lead to swipeview

--- frontend/app/src/main/java/com/quickpick/SessionActivity.java
+++ frontend/app/src/main/java/com/quickpick/SessionActivity.java
@@ -1,14 +1,53 @@
 package com.quickpick;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SessionActivity extends AppCompatActivity {
+
+    private Button foods, movies, sports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+
+        registerButtons();
+        setOnClickListeners();
+    }
+
+    private void registerButtons() {
+        foods = findViewById(R.id.food_button);
+        movies = findViewById(R.id.movie_button);
+        sports = findViewById(R.id.sport_button);
+    }
+
+    private void setOnClickListeners() {
+        foods.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SwipeActivity.class));
+            }
+        });
+
+        movies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SwipeActivity.class));
+            }
+        });
+
+        sports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), SwipeActivity.class));
+            }
+        });
+
     }
 }
---
+++ frontend/app/src/main/java/com/quickpick/SwipeActivity.java
@@ -0,0 +1,26 @@
+package com.quickpick;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SwipeActivity extends AppCompatActivity {
+
+    private Button foods, movies, sports;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+
+    }
+
+    private void registerButtons() {
+    }
+
+    private void setOnClickListeners() {
+    }
+}
