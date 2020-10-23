ad5d379b5a257420d8b63b7a4b12e24b32a492a9
Richard Echegaray <richardechegaray@outlook.com>
2020-10-22 23:47:09 -0700

    removed unused imports

--- frontend/app/src/main/java/com/quickpick/SwipeActivity.java
+++ frontend/app/src/main/java/com/quickpick/SwipeActivity.java
@@ -1,10 +1,6 @@
 package com.quickpick;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SwipeActivity extends AppCompatActivity {
