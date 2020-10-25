9a446a29186faa54d7348188147f46cbdae125f2
Thomas <thomaslapadat@gmail.com>
2020-10-24 22:51:48 -0700

    Removed Multer module, and added firebase-admin to the package files

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,6 @@ admin.initializeApp({
 //Initialize express
 const app = require('express')()
 const bodyParser = require('body-parser')
-const multer = require('multer') // v1.0.5
-const upload = multer() // for parsing multipart/form-data
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
