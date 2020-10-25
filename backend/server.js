281b7cad28dd2d7964e4ea55ffc4f36cca3d2898
afyffe <andrew.fyffe@hotmail.com>
2020-10-24 22:39:18 -0700

    Merge in session joining

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,14 @@ const USER_COLLECTION = "users"
 const mongourl = "mongodb://localhost:27017/";
 const dbname = "quickpick";
 
+
+/*
+---------Initialization
+*/
 //Initialize Firebase
 var admin = require("firebase-admin");
 // Path to secret
 var serviceAccount = require("./quickpick-7f20f-firebase-adminsdk-hvb4p-96107c2f64.json");
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://quickpick-7f20f.firebaseio.com"
@@ -22,22 +25,50 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const multer = require('multer') // v1.0.5
 const upload = multer() // for parsing multipart/form-data
-
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Initialize mongodb
+var MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(mongourl);
+
+
+/*
+----------Helper functions
+*/
 //Random string helper
+//Params: length, chars
+//Returns: string
 function randomString(length, chars) {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
 }
 
+//Helper function for sending firebase messages
+function sendFirebase(session){
+    //Get firebase tokens of everyone in the session
+    var registrationTokens = [];
+    for(i = 0; i < session.participants.length; i++){
+        db.collection(USER_COLLECTION).findOne({"id": session.participants[i].id}, function(err, res){
+            registrationTokens.push(res[0].firebaseToken)
+            })
+        }    
+    //Push messsage
+    const message = {
+        data: {"type":"session", "results": session},
+        tokens: registrationTokens
+    }
+    admin.messaging().sendMulticast(message)
+    .then((response) => { 
+        console.log(response.successCount + ' session end messages were sent');
+    })
+}
 
-//Initialize mongodb and mongoose
-var MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(mongourl);
 
+/*
+--------Endpoints
+*/
 client.connect(function(err){
     if(err) throw err;
     const db = client.db(dbname)
@@ -53,8 +84,10 @@ client.connect(function(err){
     //TODO: FB authentication
     app.get('/lists', function (req, res) {
         db.collection(LISTS_COLLECTION).find({}).toArray(function(err, result){
-            if(err) res.status(400).send({"ok": false});
-            else res.status(200).send(result);
+            if(err) res.status(400).send({"ok": false, "message": "Couldn't retrieve lists"});
+            else {
+                res.status(200).send(result);
+            }
         })
     });
 
@@ -65,7 +98,7 @@ client.connect(function(err){
         var o_id = new mongo.ObjectID(req.params.id);
 
         db.collection(SESSION_COLLECTION).find({_id : o_id}).toArray(function(err, result){
-            if (err) res.status(400).send({"ok": false});
+            if (err) res.status(400).send({"ok": false, "message": "Session doesn't exist"});
             else res.status(200).send({"session": result.ops, "ok": true});
         })
     });
@@ -80,7 +113,7 @@ client.connect(function(err){
         //Create session object
         var session= {
             "pin": rString,
-            "list": {"name": "Movie Genres", "ideas": [{"name": "Horror"}, {'name': 'Comedy'}, {'name': 'Action'}]}, //TODO: not hardcoded list
+            "list": req.body.list, //{"name": "Movie Genres", "ideas": [{"name": "Horror"}, {'name': 'Comedy'}, {'name': 'Action'}]}, //TODO: not hardcoded list
             "status": "lobby",
             "creator": 0, //TODO
             "complete": 0,
@@ -96,17 +129,21 @@ client.connect(function(err){
             jsonVar[ideaname] = 0;
             resultArray.push(jsonVar);
         }
-        session.results = resultArray;        
-        res.status(201).send({"ok":true, "session": session});
+        session.results = resultArray;       
+        db.collection(SESSION_COLLECTION).insertOne(session, function(err, result) {
+            if (err) res.status(400).send({ok: false, "message": "Session couldn't be inserted into DB"});
+            else res.status(201).send({"ok":true, "session": session});
+          }); 
+        
     });
 
     //Endpoint to receive user choices for a session
-    //TODO: FB Authentication, receive choices, check if done if it is then push firebase notification
+    //TODO: FB Authentication and firebase tokens
     app.post('/session/:id/choices', function (req, res) {
         var query = {pin: req.params.id}
         db.collection(SESSION_COLLECTION).find(query).toArray(function(err, foundSessions){
             if(err || foundSessions.length == 0){
-                res.status(400).send({"ok": false});
+                res.status(400).send({"ok": false, "message": "Session doesn't exist"});
             }
             else{
                 //Iterate through responses, and also session to find idea names that match
@@ -116,49 +153,104 @@ client.connect(function(err){
                         //If they match and the response is positive, then increment the record
                         if(req.body.choices[i].idea.name == foundSessions[0].results[j].idea.name && req.body.choices[i].choice){
                             var count = foundSessions[0].results[j].score + 1;
-                            console.log(count)
                             foundSessions[0].results[j].score = count;
                         }
                     }
                 }
-
+                //Push firebase notification if everyone has submitted their results
                 var newComplete = foundSessions[0].complete + 1;
-                var newvalues = {$set: {results: foundSessions[0].results, complete: newComplete}}
+                if(newComplete == foundSessions[0].size){
+                    foundSessions[0].status = "complete"
+                    sendFirebase(foundSessions[0]);
+                    //Include the updated session status to the database update
+                    var newvalues = {$set: {results: foundSessions[0].results, complete: newComplete, status: "complete"}}
+                }
+                else{
+                    var newvalues = {$set: {results: foundSessions[0].results, complete: newComplete}}
+                }
+                
+                
                 
                 //Update database with new values
                 db.collection(SESSION_COLLECTION).updateOne(query, newvalues, function(err,result){
-                    if (err) res.status(400).send({"ok": false});
+                    if (err) res.status(400).send({"ok": false, "message": "Couldn't update session with values"});
                     else{
                         res.status(200).send({ok: true});
                     }
                 })
-                //Push firebase notification if everyone has submitted their results
-                if(newComplete == foundSessions[0].size){
-                    //TODO: Firebase notification
-                }
+                
             }
         })
     });
 
     //Adds a user to a session
-    //TODO: FB authentication to find user??, Firebase notification to notify people
-    app.post('/session/:id', function (req, res) {
-        db.collection(SESSION_COLLECTION).find({"pin": req.params.id}).toArray(function(err, result){
-            if(err) res.status(400).send({"ok": false});
-            else{
-                //result.participants.push(json(req.body).) 
-            }
+    //TODO: FB authentication to find user??
+    app.post('/session/:id/:userid/:username', function (req, res) {
+        /* Get session matching ID */
+        db.collection(SESSION_COLLECTION).findOne({"pin": req.params.id})
+        .then((session) => {
+            
+            /* Get user matching the token that was authenticated */
+            db.collection(USER_COLLECTION).findOne({"id": req.params.userid})
+            .then((user) => {
+                
+                /* Add the user if they aren't in the session yet */
+                var flag = false;
+                for(i = 0; i < session.participants.length;i++){
+                    if(session.participants[i].name == req.params.username) flag = true;
+                }
+                if (!flag) {
+                    let newPerson = {"name": req.params.username, "id": req.params.userid};
+                    let participants = session.participants;
+                    participants.push(newPerson);
+                    db.collection(SESSION_COLLECTION)
+                    .updateOne({"pin": req.params.id}, {$set: {"participants": participants}})
+                    .then(() => {
+                        /* Update copy of session to be returned */
+                        console.log(session);
+                        /* Push firebase message to each user in the session */
+                        sendFirebase(session);
+                        res.status(201).send({"ok": true})
+                    });
+                }
+                else {
+                    res.status(400).send({"ok": false});
+                }
+            });
         })
-    });
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({"ok" : false});
+        })
+    })
 
     //Starts and runs a session
-    //TODO: FB authentication, check if user is creator of session, push firebase notification to all devices that it started
+    //TODO: FB authentication, check if user is creator of session
     app.put('session/:id', function(req, res){
-        db.collection(SESSION_COLLECTION).find({"id": req.params.id}).toArray(function(err, result){
-            if(err) res.status(400).send({"ok": false});
+        var query = {pin: req.params.id}
+        //Find session
+        db.collection(SESSION_COLLECTION).find(query).toArray(function(err, session){
+            if(err) res.status(400).send({"ok": false, "message": "Session doesn't exist"});
             else{ 
-                result.status = "running";
-            }   
+                //Check if session is in lobby
+                if(result[0].status == "lobby") {
+                    var newvalues = {$set: {status: "running"}}
+                    //Update session database
+                    db.collection(SESSION_COLLECTION).updateOne(query, newvalues, function(err,result){
+                        if (err) res.status(400).send({"ok": false, "message": "Couldn't update session"});
+                        else{
+                            //Respond to http request and send firebase notification
+                            res.status(200).send({ok: true});
+                            session[0].status = "running";
+                            sendFirebase(session[0]);
+                        }       
+                    })
+                }
+                else{
+                    res.status(400).send({"ok": false, "message": "Session has already started"})
+                }
+            }
+            
         })
     });
 
