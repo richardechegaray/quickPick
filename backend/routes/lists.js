c270ffdd91948dc629dc7111a70051b0786b5c57
Thomas <thomaslapadat@gmail.com>
2020-11-07 23:44:10 +0000

    Updated list endpoints to return ListJSON's rather than nesting them in a response object

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -8,46 +8,81 @@ const auth = require("../middleware/authentication");
 const loginHelper = require("../helpers/loginhelper");
 const imgUtil = require("../plugins/unsplash");
 
+/* Returns all lists where the userID field matches the user making the request */
 router.get("/", auth.checkFB, async function (req, res) {
     console.log("DEBUG: Get request to lists");
     try {
         const myLists = await db.collection(process.env.LISTS_COLLECTION)
-        .find({ userID: res.locals.id }).toArray();
-        console.log(myLists);
-        let listResponseObj = {};
-        let count = 1;
-        
-        myLists.forEach((doc) => {
-            listResponseObj[String(count)] = doc;
-            count++;
-        })
-        listResponseObj.ok = true;
+            .find({ userID: res.locals.id }).toArray();
+
+        let listResponseObj = { lists: [] };
+
+        myLists.forEach((ideaList) => {
+            listResponseObj.lists.push(ideaList);
+        });
+
         res.status(200).send(listResponseObj);
     }
     catch (err) {
         console.log(err);
-        res.status(500).send({ "ok": false });
+        res.status(500).send({});
     }
 });
 
+/*  */
 router.post("/", auth.checkFB, async function (req, res) {
     console.log("DEBUG: Post request to lists");
     let newList = req.body.list;
-    try {
 
-        for (let i = 0; i < newList.ideas.length; i++) {
-            let imgUrl = await imgUtil.getImage(newList.ideas[i].name);
-            // Right now we only return one image
-            newList.ideas[i].picture = imgUrl; 
+    /* newList cannot be null*/
+    if (newList === null) {
+        console.log("DEBUG: List in body is null");
+        res.status(400).send({});
+    }
+    else {
+        try {
+            /* Add an image url to each idea on the list */
+            for (let i = 0; i < newList.ideas.length; i++) {
+                let imgUrl = await imgUtil.getImage(newList.ideas[i].name);
+                newList.ideas[i].picture = imgUrl;
+            }
+            /* Set user making request as the list's owner */
+            newList.userID = res.locals.id;
+
+            await db.collection(process.env.LISTS_COLLECTION).insertOne(newList);
+            res.status(201).send(newList);
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send({});
+        }
+    }
+});
+
+/* Finds a list matching the id and returns it if the the list belongs to the user */
+router.get("/:id", auth.checkFB, async function (req, res) {
+    console.log("DEBUG: Get request to lists");
+    try {
+        /* Find the list matching the id */
+        const myList = await db.collection(process.env.LISTS_COLLECTION)
+            .findOne({ _id: req.params.id });
+
+        if (myList == null) {
+            console.log(`DEBUG: Did not find a list matching _id: ${req.params.id}`);
+            res.status(404).send({});
+        }
+        else if (myList.userID !== res.locals.id) {
+            console.log("DEBUG: Attempted to access another user's list");
+            res.status(403).send({});
+        }
+        else {
+            res.status(200).send(myList);
         }
 
-        newList.userID = res.locals.id;
-        await db.collection(process.env.LISTS_COLLECTION).insertOne(newList);
-        res.status(201).send({ "ok": true });
     }
     catch (err) {
         console.log(err);
-        res.status(500).send({ "ok": false });
+        res.status(500).send({});
     }
 });
 
