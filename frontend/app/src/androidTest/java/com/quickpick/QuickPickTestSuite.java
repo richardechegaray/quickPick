3c09492641416cf3a95796bad97604e150d76a23
Paul Lin <linpaul0328@gmail.com>
2020-11-15 02:43:49 -0800

    Add SwipeTestCase to QuickPickTestSuite
    Add test case to suite to be ran. Also move test case to its own package,
    and cleaned up unused imports

--- frontend/app/src/androidTest/java/com/quickpick/QuickPickTestSuite.java
+++ frontend/app/src/androidTest/java/com/quickpick/QuickPickTestSuite.java
@@ -2,11 +2,12 @@ package com.quickpick;
 
 import com.quickpick.login.LoginTestSuite;
 import com.quickpick.session.JoinSessionInvalidTestCase;
+import com.quickpick.swipe.SwipeTestCase;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({LoginTestSuite.class, JoinSessionInvalidTestCase.class})
+@Suite.SuiteClasses({LoginTestSuite.class, JoinSessionInvalidTestCase.class, SwipeTestCase.class})
 public class QuickPickTestSuite {
 }
