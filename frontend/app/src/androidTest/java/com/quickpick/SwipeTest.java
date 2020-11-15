cd009b3f13c7f0d9323b999e95c2c241b2776a46
Richard Echegaray <richard0108@hotmail.com>
2020-11-14 22:30:17 -0800

    test framwork done, adding asserts

---
+++ frontend/app/src/androidTest/java/com/quickpick/SwipeTest.java
@@ -0,0 +1,213 @@
+package com.quickpick;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.GeneralSwipeAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Swipe;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import com.mindorks.placeholderview.annotations.swipe.SwipeView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SwipeTest {
+
+    private static ViewAction swipeRight() {
+        return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT,
+                GeneralLocation.CENTER_RIGHT, Press.FINGER);
+    }
+    private static ViewAction swipeLeft() {
+        return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_RIGHT,
+                GeneralLocation.CENTER_LEFT, Press.FINGER);
+    }
+
+    private static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @LargeTest
+    @Test
+    public void swipeTest() { //starts at main activity
+
+        // create session
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.create_session_button), withText("Create Session"),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        sleep(1000);
+
+       // selects option button
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.session_list_edit_text),
+                        isDisplayed()));
+        textInputEditText.perform(click());
+
+        sleep(1000);
+
+        // selects movie genre
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(allOf(withId(R.id.select_dialog_listview),
+                        childAtPosition(
+                                withId(R.id.contentPanel),
+                                0)))
+                .atPosition(1);
+        appCompatCheckedTextView.perform(click());
+
+        sleep(1000);
+
+        // selects done button
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("Select"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                3)));
+        materialButton2.perform(scrollTo(), click());
+
+        sleep(1000);
+
+        // starts swiping
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.start_swiping_button), withText("Start Swiping!"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        sleep(1000);
+
+        // like button
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.likeButton), withContentDescription("like button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.FrameLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        sleep(1000);
+
+        // dislike button
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.dislikeButton), withContentDescription("dislike button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.FrameLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        sleep(1000);
+
+        // write swipe tests x7
+        onView(withIndex(withId(R.id.card_view), 0)).perform(swipeRight());
+        for (int i = 0 ; i < 3 ; i++) {
+            sleep(1000);
+            onView(withIndex(withId(R.id.card_view), 0)).perform(swipeLeft());
+            sleep(1000);
+            onView(withIndex(withId(R.id.card_view), 0)).perform(swipeRight());
+        }
+
+        sleep(1000);
+
+//        Assert.assertSame(R.id.card_view, onView(R.id.card_view));
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.return_to_main_activity_button), withText("Return to Main Menu"),
+                        childAtPosition(
+                                childAtPosition( // error here
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
