f8009584395442b8081b985fc1d7a29c6f42d828
Richard Echegaray <richard0108@hotmail.com>
2020-11-22 18:51:55 -0800

    added basic ui for creating lists

--- frontend/app/src/main/java/com/quickpick/CreateNewListActivity.java
+++ frontend/app/src/main/java/com/quickpick/CreateNewListActivity.java
@@ -1,14 +1,45 @@
 package com.quickpick;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.facebook.AccessToken;
+import com.quickpick.repositories.RunnableUtils;
+import com.quickpick.repositories.SessionRepository;
 
 public class CreateNewListActivity extends AppCompatActivity {
+
+    private Button addEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_list);
+
+        addEntries = findViewById(R.id.create_list_add_entries_button);
+
+        addEntries.setOnClickListener(view -> addEntriesDialog());
+    }
+
+    private void addEntriesDialog() {
+        AlertDialog dialog = new AlertDialog.Builder(CreateNewListActivity.this)
+                .setTitle(getString(R.string.add_entries_title))
+                .setView(R.layout.dialog_add_entries)
+                .create();
+        dialog.show();
+
+//        EditText sessionCode = dialog.findViewById(R.id.session_code_edit_text);
+//        Button cancelButton = dialog.findViewById(R.id.dialog_cancel_button);
+//        Button joinButton = dialog.findViewById(R.id.dialog_join_button);
+//
+//        if (cancelButton != null) {
+//            cancelButton.setOnClickListener(button -> dialog.dismiss());
+//        }
+
     }
 }
