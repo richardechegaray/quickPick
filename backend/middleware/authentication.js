4ab08f8fac5aaaa456526de9ef36e1b856890bdf
thomaslapadat <thomaslapadat@gmail.com>
2020-11-24 03:01:49 -0800

    Fixed codacy error

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -6,17 +6,13 @@ module.exports = {
         const facebookToken = req.headers.facebooktoken;
         const url = `https://graph.facebook.com/debug_token?input_token=${facebookToken}&access_token=${process.env.FB_APP_ID}|${process.env.FB_APP_SECRET}`;
 
-        try {
-            const facebookResponse = await axios.get(url);
-            if (facebookResponse.data.data.is_valid) {
-                res.locals.id = facebookResponse.data.data.user_id;
-                next();
-            }
-            else {
-                throw "Invalid Token";
-            }
+        const facebookResponse = await axios.get(url);
+        
+        if (facebookResponse.data.data.is_valid) {
+            res.locals.id = facebookResponse.data.data.user_id;
+            next();
         }
-        catch {
+        else {
             res.status(401).send({ message: "Failed to authenticate Facebook token" });
         }
     }
