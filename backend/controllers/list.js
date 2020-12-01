cefa15498bcb1c17f41305eee9e695504c99e2f2
thomaslapadat <thomaslapadat@gmail.com>
2020-12-01 14:44:20 -0800

    Reverted List changes because it didn't affect Codacy

--- backend/controllers/list.js
+++ backend/controllers/list.js
@@ -11,29 +11,17 @@ function isOwnedByUser(list, access, res) {
 function checkListAccess(list, access, res) {
     /* Check that the list exists */
     if (!list) {
-        Console.warn(`Did not find a list matching _id: ${res.locals.id}`);
+		Console.warn(`Did not find a list matching _id: ${res.locals.id}`);
         res.status(404).send({});
         return false;
-    }
+    } 
     /* User must own the list, unless they are trying to read a default list */
     else if (!isOwnedByUser(list, access, res)) {
-        Console.warn("Attempted to access another user's list");
+		Console.warn("Attempted to access another user's list");
         res.status(403).send({});
         return false;
     }
     return true;
-}
-
-/* Checks that the ideas passed to updateList are not null */
-function checkIdeasExist(updates, res) {
-    if (!updates || !updates.ideas) {
-        Console.warn("List in body is null");
-        res.status(400).send({});
-        return false;
-    }
-    else {
-        return true;
-    }
 }
 
 module.exports = {
@@ -58,7 +46,7 @@ module.exports = {
 
         /* newList cannot be null*/
         if (!newList) {
-            Console.warn("List in body is null");
+			Console.warn("List in body is null");
             res.status(400).send({});
         }
         else {
@@ -74,20 +62,26 @@ module.exports = {
             res.status(201).send(newList);
         }
     },
-
+    
     /* Takes the list from the request body updates the list matching the id */
     updateList: async (req, res) => {
         const myList = await List.findById(req.params.id).catch(() => null);
+        
+        /* updates cannot be null*/
         const updates = req.body.list;
+        if (!updates || !updates.ideas) {
+			Console.warn("List in body is null");
+            res.status(400).send({});
+            return;
+        }
 
-        /* Must have access to a valid list, with non-null updates*/
-        if (checkListAccess(myList, "write", res) && checkIdeasExist(updates, res)) {
+        if (checkListAccess(myList, "write", res)){
             /* Add an image url to each idea on the list */
             for (let i = 0; i < updates.ideas.length; i++) {
                 const imgUrl = await imgUtil.getImage(updates.ideas[parseInt(i, 10)].name);
                 updates.ideas[parseInt(i, 10)].picture = imgUrl;
             }
-
+            
             /* Update the name and description too if there are changes present */
             const myName = (updates.name) ? updates.name : myList.name;
             const myDesc = updates.description;
@@ -100,7 +94,7 @@ module.exports = {
                 }
             };
             await List.findByIdAndUpdate(req.params.id, newValues);
-
+            
             /* Add updates to returned list */
             myList.ideas = updates.ideas;
             myList.description = myDesc;
@@ -113,12 +107,12 @@ module.exports = {
     getList: async (req, res) => {
         /* Find the list matching the id */
         const myList = await List.findById(req.params.id).catch(() => null);
-
+        
         if (checkListAccess(myList, "read", res)) {
             res.status(200).send(myList);
         }
     },
-
+    
     /* Deletes a list matching the id if the the list belongs to the user */
     deleteList: async (req, res) => {
         /* Find the list matching the id */
